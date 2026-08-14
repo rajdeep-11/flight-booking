616eccc9a68c498a165a1e910d3eb0e412ef63a9
rajdeep-11 <rajdeepmandal841@gmail.com>
2026-08-15 00:35:15 +0530

    feat:add seats model and references

---
+++ src/utils/common/enums.js
@@ -0,0 +1,11 @@
+const SEAT_TYPE = {
+    BUSINESS : 'business',
+    ECONOMY: 'economy',
+    PREMINUM_ECONOMY : 'premium_economy',
+    FIRST_CLASS : 'first_class'
+}
+
+module.exports = {
+    
+    SEAT_TYPE
+}
