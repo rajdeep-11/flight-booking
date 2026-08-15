d2e429d792e06c967b114321bb5b90b39a2dc73f
rajdeep-11 <rajdeepmandal841@gmail.com>
2026-08-15 10:46:10 +0530

    feat: add association for Seats in Airplane model

--- src/models/airplane.js
+++ src/models/airplane.js
@@ -14,6 +14,11 @@ module.exports = (sequelize, DataTypes) => {
         foreignKey: 'airplaneId',
         onDelete: 'CASCADE'
       });
+
+      this.hasMany(models.Seat, {
+        foreignKey: 'airplaneId',
+        onDelete: 'CASCADE'
+      })
     }
   }
   Airplane.init({
